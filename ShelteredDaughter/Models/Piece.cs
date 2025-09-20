af473a82d85058c0320fe2dcac2350df4232db32
masayahak <masayahak@gmail.com>
2025-09-21 08:01:36 +0900

    プロジェクト ファイルを追加します。

---
+++ ShelteredDaughter/Models/Piece.cs
@@ -0,0 +1,22 @@
+﻿namespace ShelteredDaughter.Models
+{
+    public class Piece
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string ColorClass { get; set; }  // ← 追加
+
+        public Piece(string name, int x, int y, int w, int h, string colorClass = "piece-default")
+        {
+            Name = name;
+            X = x;
+            Y = y;
+            Width = w;
+            Height = h;
+            ColorClass = colorClass;
+        }
+    }
+}
